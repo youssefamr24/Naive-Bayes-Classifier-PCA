b0d1128fac83f2445eb7f5efc24480f8429d42e6
youssefamr24 <youssef.amrfcai@gmail.com>
2026-03-11 22:33:08 +0200

    Experiments are done in the first Dataset.

--- models.py
+++ models.py
@@ -22,6 +22,7 @@ class PCA_Scratch:
         # Sort components by eigencalues in descending order
         sorted_indices = np.argsort(eigenvalues)[::-1]
         sorted_eigenvectors = eigenvectors[:, sorted_indices]
+        self.explained_variance_ratio = eigenvalues[sorted_indices] / np.sum(eigenvalues)
 
         # Store the top components
         # The slicing starts from the top until the number of components I specified
@@ -31,69 +32,71 @@ class PCA_Scratch:
     def transform(self,X):
         X_centered = X - self.mean
         return np.dot(X_centered, self.components)
-    
+
     def fit_transform(self, X):
         self.fit(X)
         return self.transform(X)
     
 
 
-    class NaiveBayes_Scratch:
-        def __init__(self, model_type = 'gaussian'):
-            self.model_type = model_type
-            self.priors = {}
-            self.parameters = {}
-        
-        def fit(self, X, y):
-            n_samples, n_features = X.shape
-            self.classes = np.unique(y)
+class NaiveBayes_Scratch:
+    def __init__(self, model_type='gaussian'):
+        self.model_type = model_type
+        self.priors = {}
+        self.parameters = {}
+        self.classes = None
 
-            for c in self.classes:
-                X_c = X[y == c]
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.classes = np.unique(y)
 
-                # Calculate the Prior
-                self.priors[c] = X_c.shape[0] / n_samples
+        for c in self.classes:
+            X_c = X[y == c]
 
-                # Calculate the Parameters for the Categorical & Gaussain Numerical
-                if self.model_type == 'gaussian':
-                    self.parameters[c] = {
-                        "mean": X_c.mean(axis = 0),
-                        "var" : X_c.var(axis = 0) + 1e-9 # Added small epilson to avoid division by zero
-                    }
-                else:
-                    self.parameters[c] = {
-                        "probs": [ (X_c[:, i] == 1).mean() for i in range(n_features)]
-                    }
-        
-        def predict(self,X):
-            # Apply the Naive Bayes classification rule => P(y|X) = P(y) * P(X|y)
-            predictions = [self._predict_single(x) for x in X]
-            return np.array(predictions)
-        
+            # Calculate the Prior
+            self.priors[c] = X_c.shape[0] / n_samples
 
-        def _predict_single(self, x):
-            posteriors = []
-            
-            for c in self.classes:
-                prior = np.log(self.priors[c])
-
-                likelihood = np.sum(self._get_likelihood(c, x))
-                posteriors.append(prior + likelihood)
-            return self.classes[np.argmax(posteriors)]
-        
-        def _get_likelihood(self, class_idx, x):
+            # Calculate Parameters for Categorical & Gaussian
             if self.model_type == 'gaussian':
-                mean = self.parameters[class_idx]["mean"]
-                var = self.parameters[class_idx]["var"]
-
-                # Gaussian Formula
-                numerator = np.exp(-((x - mean) ** 2) / (2 * var))
-                denominator = np.sqrt(2 * np.pi * var)    
-                return np.log(numerator / denominator)
-            
+                self.parameters[c] = {
+                    "mean": X_c.mean(axis=0),
+                    "var": X_c.var(axis=0) + 1e-9
+                }
             else:
-                probs = self.parameters[class_idx]["probs"]
-                return x * np.log(probs + 1e-9) + (1 - x) * np.log(1 - probs + 1e-9)
+                self.parameters[c] = {
+                    "probs": [(X_c[:, i] == 1).mean() for i in range(n_features)]
+                }
+
+    def predict(self, X):
+        # Apply the Naive Bayes classification rule
+        predictions = [self._predict_single(x) for x in X]
+        return np.array(predictions)
+
+    def _predict_single(self, x):
+        posteriors = []
+        
+        for c in self.classes:
+            prior = np.log(self.priors[c])
+            likelihood = np.sum(self._get_likelihood(c, x))
+            posteriors.append(prior + likelihood)
+            
+        return self.classes[np.argmax(posteriors)]
+
+    def _get_likelihood(self, class_idx, x):
+        if self.model_type == 'gaussian':
+            mean = self.parameters[class_idx]["mean"]
+            var = self.parameters[class_idx]["var"]
+
+            # Gaussian Formula
+            numerator = np.exp(-((x - mean) ** 2) / (2 * var))
+            denominator = np.sqrt(2 * np.pi * var)    
+            return np.log(numerator / denominator)
+        
+        else:
+            # Convert list to numpy array for vectorized math
+            probs = np.array(self.parameters[class_idx]["probs"])
+            # Bernoulli Likelihood
+            return x * np.log(probs + 1e-9) + (1 - x) * np.log(1 - probs + 1e-9)
 
 
 
